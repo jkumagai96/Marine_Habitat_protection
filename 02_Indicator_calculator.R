00537fa7170d6b97cc07c665debc543617baca2f
Fabbiologia <favoretto.fabio@gmail.com>
2021-02-11 10:32:29 -0700

    commented script

--- 02_Indicator_calculator.R
+++ 02_Indicator_calculator.R
@@ -1,19 +1,38 @@
-library(tidyverse)
-library(stringr)
+
+# Loading libraries -------------------------------------------------------
+
+library(tidyverse) # Easily Install and Load the 'Tidyverse'
+library(stringr) # Simple, Consistent Wrappers for Common String Operations
+library(janitor) # Simple Tools for Examining and Cleaning Dirty Data
+
+# Data loading ------------------------------------------------------------
+
+# GDP data
 GDP <- read.csv('data/GDP.csv') %>% 
   select(ISO, GDP_last) %>% 
   rename(ISO_SOV1 = ISO)
 
-perc <- read.csv('data/percent_protected_boundaries.csv')
-perc_world <- read.csv('data/percent_protected_world.csv')
+# landlocked countries
 landlocked <- read.csv('data/landlocked.csv') %>% 
-  janitor::clean_names()
+  clean_names()
 
-landlocked <- landlocked$i_country
 
+landlocked <- landlocked$i_country # creates a vector of countries to eliminate
+
+
+# percent protection data
+perc <- read.csv('data/percent_protected_boundaries.csv')
+
+
+
+# Data wrangling ----------------------------------------------------------
+
+
+# filtering out landlocked countries
 perc <- perc %>% 
   filter(!UNION %in% landlocked)
 
+# creating habitat categories
 perc$habitats <- factor(perc$category, labels = c(
   "ColdCorals",
   "ColdCorals_allmpa",
@@ -37,6 +56,8 @@ perc$habitats <- factor(perc$category, labels = c(
   "Seagrasses_notake"
 ))
 
+
+# separating habitats and level of protection and widen the data
 perc_wide <- perc %>% 
   separate(habitats, c("habitats", "protection")) %>% 
   mutate(protection = replace_na(protection, replace = "total_area")) %>% 
@@ -44,73 +65,64 @@ perc_wide <- perc %>%
   mutate_if(is.numeric, ~replace_na(., 0))
 
 
-# I will summarise by SOVEREINITY, since it represents better the authority on the area
 
 
-(p1 <- perc %>% 
-  separate(habitats, c("habitats", "protection")) %>% 
-  mutate(protection = replace_na(protection, replace = "total_area")) %>% 
-  pivot_wider(UNION:habitats, names_from = "protection", values_from = "pixel_counts") %>% 
-  group_by(ISO_SOV1, habitats) %>% 
-  summarise_at(c("total_area", "allmpa", "managed", "notake"), sum, na.rm = TRUE) %>% 
-  merge(., GDP, by = "ISO_SOV1", all.x = TRUE) %>% 
+
+
+perc %>%
+  separate(habitats, c("habitats", "protection")) %>%
+  mutate(protection = replace_na(protection, replace = "total_area")) %>%
+  pivot_wider(UNION:habitats,
+              names_from = "protection",
+              values_from = "pixel_counts") %>%
+  group_by(ISO_SOV1, habitats) %>% # I will summarize by SOVEREINITY, since it represents better the authority on the area
+  summarise_at(c("total_area", "allmpa", "managed", "notake"), sum, na.rm = TRUE) %>%
+  merge(., GDP, by = "ISO_SOV1", all.x = TRUE) %>%
   filter(total_area > 0) %>% # Filtering ones with NO habitat
-  ungroup() %>% 
-  mutate( GDP_perc = round((GDP_last/sum(GDP_last, na.rm = T)*100), 6)) %>% 
+  ungroup() %>%
+  mutate(GDP_perc = round((GDP_last / sum(GDP_last, na.rm = T) * 100), 6)) %>%
   group_by(ISO_SOV1, habitats) %>%  #creating world stat
-  mutate(mpa_perc = (allmpa/sum(total_area))*100) %>%
-  group_by(habitats) %>% 
-  mutate(mean_effort = mean(mpa_perc),
-         median_effort = median(mpa_perc),
-         effort_gap_habitat = mean_effort - median_effort) %>%
-  group_by(ISO_SOV1, habitats) %>% 
-  mutate(effort_gap_country = mean_effort-mpa_perc) %>% 
-  select(ISO_SOV1, habitats, GDP_perc, mpa_perc, mean_effort, median_effort, effort_gap_habitat, effort_gap_country) %>% 
-  mutate(increase = mpa_perc + effort_gap_country) %>% 
-  ggplot(aes(x=habitats, y = mpa_perc, label = ISO_SOV1))+
-  geom_point(aes(x=habitats, y=mpa_perc, size = GDP_perc), col = "gray80")+
+  mutate(mpa_perc = (allmpa / sum(total_area)) * 100) %>%
+  group_by(habitats) %>%
+  mutate(
+    mean_effort = mean(mpa_perc),
+    median_effort = median(mpa_perc),
+    effort_gap_habitat = mean_effort - median_effort
+  ) %>%
+  group_by(ISO_SOV1, habitats) %>%
+  mutate(effort_gap_country = mean_effort - mpa_perc) %>%
+  select(
+    ISO_SOV1,
+    habitats,
+    GDP_perc,
+    mpa_perc,
+    mean_effort,
+    median_effort,
+    effort_gap_habitat,
+    effort_gap_country
+  ) %>%
+  mutate(increase = mpa_perc + effort_gap_country) %>%
+  ggplot(aes(x = habitats, y = mpa_perc, label = ISO_SOV1)) +
+  geom_point(aes(x = habitats, y = mpa_perc, size = GDP_perc), col = "gray80") +
   #geom_point(col = "gray50")+
-  geom_point(aes(x=habitats, y=mean_effort), col = "blue", size = 2)+
-  geom_point(aes(x=habitats, y=median_effort), col = "red", size = 2)+
-  geom_hline(yintercept = 30)+
-  geom_text(size=2)+
-  coord_flip()+
-  theme_bw())
+  geom_point(aes(x = habitats, y = mean_effort),
+             col = "blue",
+             size = 2) +
+  geom_point(aes(x = habitats, y = median_effort),
+             col = "red",
+             size = 2) +
+  geom_hline(yintercept = 30) +
+  #geom_text(size = 2) +
+  coord_flip() +
+  theme_bw()
   
 
-(p2 <- perc %>% 
-    separate(habitats, c("habitats", "protection")) %>% 
-    mutate(protection = replace_na(protection, replace = "total_area")) %>% 
-    pivot_wider(UNION:habitats, names_from = "protection", values_from = "pixel_counts") %>% 
-    group_by(ISO_SOV1, habitats) %>% 
-    summarise_at(c("total_area", "allmpa", "managed", "notake"), sum, na.rm = TRUE) %>% 
-    merge(., GDP, by = "ISO_SOV1", all.x = TRUE) %>% 
-    filter(total_area > 0) %>% # Filtering ones with NO habitat
-    ungroup() %>% 
-    mutate( GDP_perc = round((GDP_last/sum(GDP_last, na.rm = T)*100), 6)) %>% 
-    group_by(ISO_SOV1, habitats) %>%  #creating world stat
-    mutate(mpa_perc = (allmpa/sum(total_area))*100) %>%
-    group_by(habitats) %>% 
-    mutate(mean_effort = mean(mpa_perc),
-           median_effort = median(mpa_perc),
-           effort_gap_habitat = mean_effort - median_effort) %>%
-    group_by(ISO_SOV1, habitats) %>% 
-    mutate(effort_gap_country = mean_effort-mpa_perc) %>% 
-    select(ISO_SOV1, habitats, GDP_perc, mpa_perc, mean_effort, median_effort, effort_gap_habitat, effort_gap_country) %>% 
-    mutate(increase = mpa_perc + effort_gap_country) %>% 
-    ggplot(aes(x=habitats, y = increase, label = ISO_SOV1))+
-    geom_point(aes(x=habitats, y = increase, size = GDP_perc), col = "gray80")+
-    geom_point()+
-    #geom_point(col = "gray50")+
-    #geom_point(aes(x=habitats, y=mean_effort), col = "blue", size = 2)+
-    #geom_point(aes(x=habitats, y=median_effort), col = "red", size = 2)+
-    geom_hline(yintercept = 30)+
-    coord_flip()+
-    theme_bw())
+ 
 
 
 
 
+# Saving effort gap table
 
 perc %>% 
   separate(habitats, c("habitats", "protection")) %>% 
@@ -134,15 +146,38 @@ perc %>%
   write.csv(., 'effort_gap.csv', row.names = F)
 
 
-
-
-  
-  
-  
-  
-  
-
 # Experimentation ---------------------------------------------------------
+
+perc %>% 
+  separate(habitats, c("habitats", "protection")) %>% 
+  mutate(protection = replace_na(protection, replace = "total_area")) %>% 
+  pivot_wider(UNION:habitats, names_from = "protection", values_from = "pixel_counts") %>% 
+  group_by(ISO_SOV1, habitats) %>% 
+  summarise_at(c("total_area", "allmpa", "managed", "notake"), sum, na.rm = TRUE) %>% 
+  merge(., GDP, by = "ISO_SOV1", all.x = TRUE) %>% 
+  filter(total_area > 0) %>% # Filtering ones with NO habitat
+  ungroup() %>% 
+  mutate( GDP_perc = round((GDP_last/sum(GDP_last, na.rm = T)*100), 6)) %>% 
+  group_by(ISO_SOV1, habitats) %>%  #creating world stat
+  mutate(mpa_perc = (allmpa/sum(total_area))*100) %>%
+  group_by(habitats) %>% 
+  mutate(mean_effort = mean(mpa_perc),
+         median_effort = median(mpa_perc),
+         effort_gap_habitat = mean_effort - median_effort) %>%
+  group_by(ISO_SOV1, habitats) %>% 
+  mutate(effort_gap_country = mean_effort-mpa_perc) %>% 
+  select(ISO_SOV1, habitats, GDP_perc, mpa_perc, mean_effort, median_effort, effort_gap_habitat, effort_gap_country) %>% 
+  mutate(increase = mpa_perc + effort_gap_country) %>% 
+  ggplot(aes(x=habitats, y = increase, label = ISO_SOV1))+
+  geom_point(aes(x=habitats, y = increase, size = GDP_perc), col = "gray80")+
+  geom_point()+
+  #geom_point(col = "gray50")+
+  #geom_point(aes(x=habitats, y=mean_effort), col = "blue", size = 2)+
+  #geom_point(aes(x=habitats, y=median_effort), col = "red", size = 2)+
+  geom_hline(yintercept = 30)+
+  coord_flip()+
+  theme_bw()
+
 perc %>% 
   separate(habitats, c("habitats", "protection")) %>% 
   mutate(protection = replace_na(protection, replace = "total_area")) %>% 
@@ -190,3 +225,9 @@ perc %>%
     ggplot(aes(x = habitats, y = mpa_rel_to_world, fill = ISO_SOV1)) +
     geom_col() +
     theme(legend.position = "")
+
+
+
+# END OF SCRIPT -----------------------------------------------------------
+
+
