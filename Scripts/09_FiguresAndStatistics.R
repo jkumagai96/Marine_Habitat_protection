3494d5cfc92c7215d40a5b34403e1a3a8109e8ab
Joy Kumagai <49419490+jkumagai96@users.noreply.github.com>
2021-03-02 17:21:46 +0100

    ideas

--- Scripts/09_FiguresAndStatistics.R
+++ Scripts/09_FiguresAndStatistics.R
@@ -1,55 +1,62 @@
 # Joy Kumagai 
-# Date: Dec 2020
-# Figures V1 
+# Date: Narch 2021
+# Figures 
 # Marine Habitat Protection Indicator
 
-#### Load Packages ####
-library(tidyverse)
+##### Load Packages #####
 library(sf)
-library(RColorBrewer)
+library(rnaturalearth)
+library(graticule) 
 
-#### Load Data ####
-poly <- read_sf("data/data/EEZ_Land/EEZ_Land_v3_202030.shp")
-countries <- read_sf("data/data/countries/Country_Polygon.shp")
-data_world <- read.csv("data/percent_protected_world.csv")
-data_countries <- read.csv("data/percent_protected_boundaries.csv")
-data_countries <- data_countries[!is.na(data_countries$percent_protected),]
+##### Load Data ####
+data <- read.csv("Data_final/percent_protected_boundaries.csv")
+data_world <- read.csv("Data_final/percent_protected_world.csv")
+eez_land <- read_sf("Data_original/eez_land/EEZ_Land_v3_202030.shp")
 
-#### Basic Statistics ####
-# Number of countries/territories with data per habitat
-length(unique(data_countries$ISO_TER1)) # territories
-length(unique(data_countries$ISO_SOV1)) # countries 
+land <- ne_countries(scale = 110, returnclass = "sf")
 
-#### Figures ####
-# Map of countries with average percentage of key habitats protected
-data <- data_countries %>% 
-  mutate(match = grepl(pattern = "(All_mpas)$", data_countries$category)) %>% 
-  filter(match == TRUE) %>% 
-  dplyr::select(category, percent_protected:ISO_SOV1) %>% 
-  group_by(UNION) %>% 
-  summarise(percent_protected_average = mean(percent_protected)) 
+##### Formating Data #####
 
-robin <-  "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
+# Projecting data
+robin <- "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
+land <- st_transform(land, crs = robin)
+land_l <- st_cast(land,"POLYGON")
+land_t <- st_cast(land_l, "LINESTRING")
+eez_land <- st_transform(eez_land, crs = robin)
 
-poly_data <- full_join(data, poly, by = "UNION") %>% 
-  st_as_sf() %>% 
-  st_transform(crs = robin)
-countries <- st_transform(countries, crs = robin)
+# Join the indicator data onto the eez_land 
 
-my.palette <- colorRampPalette(brewer.pal(7, "Purples"))(10)
+##### Figure 1 ######
+# Two maps with average percent protection by country for A) no-take B) all MPAs
 
-plot(poly_data[,2], main = NULL, graticule = TRUE, axes = TRUE, pal = my.palette, key.pos = NULL)
-plot(countries[,1], main = NULL, graticule = TRUE, axes = TRUE, col = "lightgrey")
+# Create graticules 
+# Creates latitude and longitude labels and graticules
+lat <- c(-90, -60, -30, 0, 30, 60, 90)
+long <- c(-180, -120, -60, 0, 60, 120, 180)
+labs <- graticule::graticule_labels(lons = long, lats = lat, xline = -180, yline = 90, proj = robin) # labels for the graticules 
+lines <- graticule::graticule(lons = long, lats = lat, proj = robin) # graticules 
 
-ggplot() +
-  geom_sf(data = poly_data[,2], fill = poly_data$percent_protected_average) 
+# plotting 
 
-# Map of countries with total area of key habitats protected 
+png("figure1.png", units="in", width=8, height=10, res=600)
 
+par(mfrow=c(2,1))
+# plot 1
+plot(lines, lty = 5, col = "grey") # plots graticules 
+text(subset(labs, labs$islon), lab = parse(text = labs$lab[labs$islon]), pos = 3, xpd = NA) # plots longitude labels
+text(subset(labs, !labs$islon), lab = parse(text = labs$lab[!labs$islon]), pos = 2, xpd = NA) # plots latitude labels
+raster::plot(eez_land[,6], col = "grey", border = "transparent", add = TRUE)
+raster::plot(land[,1], col = "transparent", border = "black", add = TRUE)
+title("A", adj = 0.1, line = -1, cex.main = 2)
 
-# Figure of global habitat protection (with values of countries as points) 
+# plot 2
+plot(lines, lty = 5, col = "grey") # plots graticules 
+text(subset(labs, labs$islon), lab = parse(text = labs$lab[labs$islon]), pos = 3, xpd = NA) # plots longitude labels
+text(subset(labs, !labs$islon), lab = parse(text = labs$lab[!labs$islon]), pos = 2, xpd = NA) # plots latitude labels
+raster::plot(eez_land[,6], col = "lightblue", border = "transparent", add = TRUE)
+raster::plot(land[,1], col = "transparent", border = "black", add = TRUE)
+title("B", adj = 0.1, line = -1, cex.main = 2)
 
-
-# Figure of effort gap??
-
-
+dev.off()
+##### Figure 2 #####
+# Stacked bar graph of protection for each habitat included 
