fa650026787afcb15874f46dd805a2b0b8428342
Joy Kumagai <49419490+jkumagai96@users.noreply.github.com>
2021-03-03 19:38:15 +0100

    Practice figures

--- Scripts/09_FiguresAndStatistics.R
+++ Scripts/09_FiguresAndStatistics.R
@@ -7,6 +7,7 @@
 library(sf)
 library(rnaturalearth)
 library(graticule) 
+library(tidyverse)
 
 ##### Load Data ####
 data <- read.csv("Data_final/percent_protected_boundaries.csv")
@@ -20,12 +21,37 @@ land <- ne_countries(scale = 110, returnclass = "sf")
 # Projecting data
 robin <- "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m"
 land <- st_transform(land, crs = robin)
-land_l <- st_cast(land,"POLYGON")
-land_t <- st_cast(land_l, "LINESTRING")
 eez_land <- st_transform(eez_land, crs = robin)
 
 # Join the indicator data onto the eez_land 
+eez_land$UNION
+eez_land$testdata <- sample(1:100, 323, replace = TRUE)
+eez_land <- eez_land %>% 
+  filter(UNION != "United States", 
+         UNION != "China")
 
+
+# World Data
+habitats <- c("Cold Corals", "Coral Reefs", "Mangroves", "Saltmarsh", "Seagrasses")
+
+no_take <- data_world %>% 
+  dplyr::select(Name, percent_protected) %>% 
+  filter(grepl("No_take",Name)) %>% 
+  mutate(type = "No_take") %>% 
+  mutate(habitat = habitats)
+
+all <- data_world %>% 
+  dplyr::select(Name, percent_protected) %>% 
+  filter(grepl("All", Name)) %>% 
+  mutate(type = "All")%>% 
+  mutate(habitat = habitats)
+
+data2 <- rbind(no_take, all) %>% 
+  dplyr::select(habitat, percent_protected, type) %>% 
+  pivot_wider(names_from = type, values_from = percent_protected) %>% 
+  mutate(difference = All - No_take) %>% 
+  dplyr::select(-All) %>% 
+  pivot_longer(cols = c(No_take, difference), names_to = "type", values_to = "percent_protected") 
 ##### Figure 1 ######
 # Two maps with average percent protection by country for A) no-take B) all MPAs
 
@@ -37,15 +63,14 @@ labs <- graticule::graticule_labels(lons = long, lats = lat, xline = -180, yline
 lines <- graticule::graticule(lons = long, lats = lat, proj = robin) # graticules 
 
 # plotting 
+png("figure1.png", width = 8, height = 10.5, units = "in", res = 300)
 
-png("figure1.png", units="in", width=8, height=10, res=600)
-
-par(mfrow=c(2,1))
+par(mfcol = c(2,1))
 # plot 1
 plot(lines, lty = 5, col = "grey") # plots graticules 
 text(subset(labs, labs$islon), lab = parse(text = labs$lab[labs$islon]), pos = 3, xpd = NA) # plots longitude labels
 text(subset(labs, !labs$islon), lab = parse(text = labs$lab[!labs$islon]), pos = 2, xpd = NA) # plots latitude labels
-raster::plot(eez_land[,6], col = "grey", border = "transparent", add = TRUE)
+raster::plot(eez_land[,32], border = "transparent", add = TRUE)
 raster::plot(land[,1], col = "transparent", border = "black", add = TRUE)
 title("A", adj = 0.1, line = -1, cex.main = 2)
 
@@ -60,3 +85,11 @@ title("B", adj = 0.1, line = -1, cex.main = 2)
 dev.off()
 ##### Figure 2 #####
 # Stacked bar graph of protection for each habitat included 
+
+plot2 <- ggplot(data2, aes(x = habitat, y = percent_protected, fill = type)) +
+  geom_bar(position="stack", stat="identity") +
+  scale_fill_manual(values = c("#BADDF5","#174FB8"), labels = c("All", "No-take")) +
+  theme_minimal() +
+  labs(x = "Habtiat", y = "Percent Protection Globally", fill = "Type of Protected Areas") 
+
+ggsave("figure2.png", plot2, device = "png", width = 7, height = 5, units = "in", dpi = 600)
