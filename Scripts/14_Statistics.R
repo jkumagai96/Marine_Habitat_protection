d7875d383e1e3df02809bbc41debf6914d6cb1bc
Joy Kumagai <49419490+jkumagai96@users.noreply.github.com>
2021-04-21 10:35:58 +0200

    double checking and adding manuscript stats

--- Scripts/14_Statistics.R
+++ Scripts/14_Statistics.R
@@ -7,30 +7,76 @@
 library(tidyverse) # Easily Install and Load the 'Tidyverse'
 
 ##### Descriptive Statistics #####
-data <- read.csv("Data_final/percent_protected_boundaries.csv")
+data <- read.csv("Data_final/habitat_protection_indexes_average.csv")
+
+## Global Habitat Protection Index (top jurisdictions)
+top_5 <- data %>% 
+  slice_max(order_by = G_Hs_P_I, n = 5) %>% 
+  print()
+
+sum(top_5$G_Hs_P_I)
+
+## Counts for L_Hs_P_I (Local Habitat Protection Index)
+n <- data %>% 
+  drop_na(L_Hs_P_I) %>% 
+  count() %>% 
+  print()
 
 n_zero <- data %>% 
-  filter(pp_mean_all == 0) %>% 
-  count()/(length(unique(data$habitat)))
+  filter(L_Hs_P_I == 0) %>% 
+  count() %>% 
+  print()
 
 n_hundred <- data %>% 
-  filter(pp_mean_all == 100) %>% 
-  count()/(length(unique(data$habitat)))
+  filter(L_Hs_P_I == 1) %>% 
+  count() %>% 
+  print()
 
-n <- data %>% 
-  drop_na(pp_mean_all) %>% 
-  count()/(length(unique(data$habitat))) 
+## Histograms 
+# Global Habitat Protection Index 
+hist(data$G_Hs_P_I)
+hist(log(data$G_Hs_P_I))
 
-indicator <- data %>% 
-  group_by(UNION) %>% 
-  summarize(mean(pp_mean_all))
-
-colnames(indicator) <- c("Area", "Indicator")
-hist(indicator$Indicator)
-quantile(indicator$Indicator, na.rm = TRUE)
+# Local Habitat Protection Index 
+hist(data$L_Hs_P_I)
+quantile(data$L_Hs_P_I, na.rm = TRUE)
 
 data %>% 
-  filter(pp_mean_all > 50) %>% 
-  count()/(length(unique(data$habitat)))
+  filter(L_Hs_P_I > .50) %>% 
+  count()
 
+## Targeted Global Habitat Protection Index
+data %>% 
+  slice_max(order_by = T_Hs_I, n = 10) # Top 10 
 
+## Habitat Specific Counts of Jurisdictions
+data2 <- read.csv("Data_final/habitat_protection_indexes.csv")
+data2 %>% 
+  filter(habitat == "coldcorals") %>% 
+  drop_na(T_H_I) %>% 
+  count()
+
+data2 %>% 
+  filter(habitat == "coralreefs") %>% 
+  drop_na(T_H_I) %>% 
+  count()
+
+data2 %>% 
+  filter(habitat == "knolls_seamounts") %>% 
+  drop_na(T_H_I) %>% 
+  count()
+
+data2 %>% 
+  filter(habitat == "mangroves") %>% 
+  drop_na(T_H_I) %>% 
+  count()
+
+data2 %>% 
+  filter(habitat == "saltmarshes") %>% 
+  drop_na(T_H_I) %>% 
+  count()
+
+data2 %>% 
+  filter(habitat == "seagrasses") %>% 
+  drop_na(T_H_I) %>% 
+  count()
